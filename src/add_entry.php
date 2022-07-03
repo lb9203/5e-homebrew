170104fc8f12d949c38eeb403a790af4fdc000ae
Leon Blejc <leon.blejc@gmail.com>
2022-07-03 03:00:05 +0200

    Add simple UI to add new entities, implement generation from multiple files.

---
+++ src/add_entry.php
@@ -0,0 +1,46 @@
+<?php
+
+const VALID_TYPES = ['monster', 'spell', 'deity'];
+
+$type = $_POST['type'];
+$json = $_POST['json'];
+
+if (($decodedJson = json_decode($json, true)) === null) {
+    http_response_code(400);
+    echo "Invalid JSON";
+    exit(400);
+}
+
+if (!in_array($type, VALID_TYPES, true)) {
+    http_response_code(400);
+    echo "Invalid type";
+    exit(400);
+}
+
+try {
+    $name = $decodedJson['name'];
+
+    if (!is_dir($directory =  sprintf('../homebrew_schema/%s', $type))) {
+        mkdir($directory);
+    }
+
+    $filename = str_replace(' ', '_', strtolower($name));
+    $filename = str_replace(',', '', $filename);
+    $filename = sprintf('%s/%s.json', $directory, $filename);
+
+    file_put_contents($filename, $json);
+
+    ob_start();
+
+    require 'generate_homebrew.php';
+
+    ob_end_clean();
+
+    exit(200);
+} catch (Throwable $throwable) {
+    echo $throwable->getMessage();
+    exit(500);
+}
+
+
+
---
+++ src/generate_homebrew.php
@@ -0,0 +1,59 @@
+<?php
+
+const HOMEBREW_DIR  = '../homebrew_schema';
+const BASE_FILE     = 'base.json';
+const HOMEBREW_FILE = 'homebrew.json';
+const PROCESS_TYPES  = [
+    'monster',
+    'deity',
+    'spell',
+    'item'
+];
+
+$json = json_decode(file_get_contents(sprintf('%s/%s', HOMEBREW_DIR, BASE_FILE)), true);
+
+foreach (PROCESS_TYPES as $type) {
+    $typeDir = sprintf('%s/%s', HOMEBREW_DIR, $type);
+
+    echo sprintf("Processing: %s\n", $typeDir);
+
+    if (!is_dir($typeDir)) {
+        continue;
+    }
+
+    $typeFiles = scandir($typeDir);
+
+    if (!$typeFiles) {
+        echo sprintf("\tError: couldn\'t scan type dir %s.\n", $typeDir);
+        continue;
+    }
+
+    $typeFiles = array_filter($typeFiles, fn(string $filename): bool => $filename !== '.' && $filename !== '..');
+
+    foreach ($typeFiles as $typeFile) {
+        $typeFileWithPath = sprintf('%s/%s', $typeDir, $typeFile);
+
+        if (pathinfo($typeFileWithPath)['extension'] !== 'json') {
+            echo sprintf("\tError: %s is not a json file.\n", $typeFile);
+            continue;
+        }
+
+        $typeFileJson = json_decode(file_get_contents($typeFileWithPath), true);
+
+        if ($typeFileJson === null) {
+            echo sprintf("\tError: %s does not contain valid json.\n", $typeFile);
+            continue;
+        }
+
+        $json[$type][] = $typeFileJson;
+
+        echo sprintf("\t%s done\n", $typeFile);
+    }
+}
+
+echo "Updating dateLastModified\n";
+
+$json['_meta']['dateLastModified'] = time();
+
+file_put_contents(sprintf('%s/%s', HOMEBREW_DIR, HOMEBREW_FILE), json_encode($json, JSON_PRETTY_PRINT));
+
